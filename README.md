329dd8957d39a57b0b47a09baf239575ddf6c7d9
Dmirtiy Martianov <dmitry.martyanov@pegast.ru>
2016-10-25 16:29:57 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Use scss starter templates
+Minimal template for a quick start , consisting of scss, compass, lite-server.
---
+++ bs-config.js
@@ -0,0 +1,23 @@
+'use strict';
+/*
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ */
+module.exports = {
+    injectChanges: true,
+    files: ['./**/*.{html,htm,css,js}'],
+    watchOptions: {
+        ignored: 'node_modules'
+    },
+    server: {
+        baseDir: [
+            './'
+        ],
+        routes: {
+            '/built': '',
+            '/app': '',
+            '/assets': '',
+            '/img': '',
+        }
+    }
+};
